be26d2abe3377e86382f9d76116eba7073acff89
nata18629 <nata18629@gmail.com>
2025-08-09 09:24:18 +0200

    bug

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,12 +1,20 @@
+#pragma once
 #include <webgpu/webgpu.hpp>
-#include <vector>
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
+// struct VertexAttributes {
+//     std::vector<float> position;
+//     std::vector<float> normal;
+//     std::vector<float> color;
+// };
+
+
 class ResourceManager {
     public:
     static wgpu::ShaderModule loadShaderModule(const fs::path& path, wgpu::Device device);
+    //static bool loadGeometryObj(const fs::path& path, std::vector<VertexAttributes>& vertexData);
 
     private:
 
